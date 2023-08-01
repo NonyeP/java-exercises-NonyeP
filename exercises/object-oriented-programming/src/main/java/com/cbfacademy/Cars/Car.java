377bc618ceb360f51fc230b6ce3ca8f391005bd0
Juliet Phelan <julietphelan@outlook.com>
2023-08-01 14:40:44 +0100

    Car exercise

---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/Cars/Car.java
@@ -0,0 +1,35 @@
+package com.cbfacademy.Cars;
+
+public class Car{
+
+    private String make;
+    private String model;
+    private int year;
+
+    
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/Cars/CarDealer.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.Cars;
+
+public class CarDealer {
+    public static void main(String[] args) {
+        Car car1 = new Car("Volvo", "V40", 2012);
+        Car car2 = new Car("Porsche", "Panamera", 2009);
+        Car car3 = new Car("Audi", "A3", 2018);
+        System.out.println(car1.getMake());
+        System.out.println(car2.getModel());
+        System.out.println(car3);
+    }
+    
+}
